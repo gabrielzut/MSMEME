8d03b284482f4f077034b3e28084ea0f4954aefa
caarlos.silva06@gmail.com <caarlos.silva06@gmail.com>
2018-11-06 04:48:40 -0200

    adicionado emotes na pagina conversa

--- conversa.php
+++ conversa.php
@@ -11,6 +11,13 @@
         crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/CSS" href="estilo.css">
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.1/emojionearea.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.1/emojionearea.min.js"></script>
+    <script type="text/javascript" src="js.js"></script>
+
+
     <title>MSMEME</title>
     <?php header('Content-Type: text/html; charset=utf-8');?>
 </head>
@@ -106,7 +113,7 @@
                         </div>
                     </div>
                     <div class="col-md-10 col-12">
-                        <textarea class="form-control my-2" rows="3" ></textarea>
+                        <textarea id="txArea" class="form-control my-2" rows="3" ></textarea>
                     </div>
                     <div class="col aligncentro">
                         <button type="submit" class="btn btn-lg">Enviar</button>
@@ -115,6 +122,9 @@
             </div>
         </div>
     </div>
+
+    
+
     <footer class="footer bg-dark">
         <div class="container">
             <span class="text-white">
@@ -124,12 +134,16 @@
     </footer>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script>
 </body>
+<script>
+    $("#txArea").emojioneArea({
+        pickerPosition:"bottom"
+    });
+</script>
 
 </html>
