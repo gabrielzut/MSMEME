90a72c70c82ff0f6518f12956c836207d38e0358
Gabriel Augusto Zutião <30675798+gabrielzut@users.noreply.github.com>
2018-12-10 10:24:03 -0200

    Update pedidos.php

--- pedidos.php
+++ pedidos.php
@@ -30,14 +30,14 @@
         <a class="navbar-brand" href="contatos.php">
             <img src="logo.png" width="120" height="33" alt="">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="collapse navbar-collapse" id="navbar">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="contatos.php">Contatos<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="contatos.php">Contatos</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="perfil.php">Minha conta</a>
